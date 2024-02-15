8e21e0acc30e46f712a32383b2c771c2ba758227
Alex Schiftner <alex@shapediver.com>
2024-02-15 20:17:47 +0100

    initial commit

---
+++ PluginGrasshopper/PluginLoader.cs
@@ -0,0 +1,19 @@
+﻿using Grasshopper;
+using Grasshopper.Kernel;
+using System;
+using System.Drawing;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace NoahsArk.PluginGrasshopper
+{
+    public class PluginLoader : GH_AssemblyPriority
+    {
+        public override GH_LoadingInstruction PriorityLoad()
+        {
+            Instances.ComponentServer.AddCategoryIcon("NoahsArk", ResourceLoader.LoadBitmap("NoahsArkIcon_16.png"));
+            Instances.ComponentServer.AddCategorySymbolName("NoahsArk", 'N');
+            return GH_LoadingInstruction.Proceed;
+        }
+    }
+}
